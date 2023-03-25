6a96fe22e27639cffbb3253127b4d8a3e84a009b
sanggyunbak2856 <baksanggyun2856@gmail.com>
2023-03-25 15:38:48 +0900

    init

---
+++ src/test/java/dpicb/alarm/AlarmApplicationTests.java
@@ -0,0 +1,13 @@
+package dpicb.alarm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlarmApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
